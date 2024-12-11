34507b0170616110247149e5b2debf96f3593420
Rammohan0000 <120706490+Rammohan0000@users.noreply.github.com>
2024-12-11 11:35:46 +0530

    Add files via upload

---
+++ pip.py
@@ -0,0 +1,15 @@
+# what is pip? ->  Python Package Installer
+# PIP is a package manager for Python. It allows you to install, update, and manage Python packages and libraries from the Python Package Index (PyPI).
+# we can uninstall packages and install specific versions of packages
+# To check the version of pip -> pip --version
+# if pip is missing -> python get-pip.py
+# to verify installed packages and dependencies -> pip list
+# to generate list of dependenicies -> pip freeze > requirements.txt(it creates a requirements.txt file)
+# to install dependencies from requirements.txt file -> pip instal -r requirements.txt
+# how to use pip to install package -> pip install <package-name>
+# how to uninstall a package using pip -> pip uninstall <package-name>
+# installing a specific version -> pip install package_name == version_number
+# upgrading a package -> pip install --upgrade <package-name>
+# to view package info -> pip show <package-name>
+# installing multiple packages from requirements.txt -> pip install -r requirements.txt
+# search for a package -> pip search <package-name>
