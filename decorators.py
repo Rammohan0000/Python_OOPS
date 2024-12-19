e188b7baf17c4b52a49f94bb154f67a857b9dc2f
Rammohan0000 <120706490+Rammohan0000@users.noreply.github.com>
2024-12-19 17:02:49 +0530

    decorator changes

--- decorators.py
+++ decorators.py
@@ -39,3 +39,51 @@ def sensitive_action():
 sensitive_action("admin")
 sensitive_action("guest")
 
+def hello_decorator(func):
+    def inner1(*args, **kwargs):
+        
+        print("before Execution")
+        
+        # getting the returned value
+        returned_value = func(*args, **kwargs)
+        print("after Execution")
+        
+        # returning the value to the original frame
+        return returned_value
+        
+    return inner1
+
+
+# adding decorator to the function
+@hello_decorator
+def sum_two_numbers(a, b):
+    print("Inside the function")
+    return a + b
+
+a, b = 1, 2
+
+# getting the value through return of the function
+print("Sum =", sum_two_numbers(a, b))
+
+# chaining decorator
+def decor1(func): 
+    def inner(): 
+        x = func() 
+        return x * x 
+    return inner 
+
+def decor(func): 
+    def inner(): 
+        x = func() 
+        return 2 * x 
+    return inner 
+@decor1
+@decor
+def num(): 
+    return 10
+@decor
+@decor1
+def num2():
+    return 10
+print(num()) 
+print(num2())
