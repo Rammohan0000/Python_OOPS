cf04db16918bdf7bcdbb5d636de5bb53941eb816
Rammohan0000 <120706490+Rammohan0000@users.noreply.github.com>
2025-01-28 18:44:02 +0530

    reg

--- Pyytest_demos/intro.py
+++ Pyytest_demos/intro.py
@@ -53,3 +53,25 @@ def test_less():
    assert num < 200
 
 # to run the tests in a class, we can use the -m option followed by the marker name.          
+# pytest -m great marker.py
+
+# Fixtures
+#Fixtures are functions, which will run before each test function to which it is applied. 
+# Fixtures are used to feed some data to the tests such as database connections, URLs to test and some sort of input data. 
+# Therefore, instead of running the same code for every test, we can attach fixture function to the tests and it will run and return the data to the test before executing each test.
+# Fixtures are defined using the pytest.fixture() decorator.
+# Fixtures are used to manage the test setup and teardown.
+# Example:
+import pytest
+@pytest.fixture
+def input_value():
+   input = 39
+   return input
+def test_divisible_by_3(input_value):
+    assert input_value % 3 == 0
+def test_divisible_by_6(input_value):
+    assert input_value % 6 == 0
+
+# we have a fixture function named input_value, which supplies the input to the tests. To access the fixture function
+# Pytest while the test is getting executed, will see the fixture name as input parameter. 
+# It then executes the fixture function and the returned value is stored to the input parameter, which can be used by the test.
