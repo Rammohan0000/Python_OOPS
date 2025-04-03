50b07d8c8b07495014f93b9c4654b5ffb20f7908
Rammohan0000 <120706490+Rammohan0000@users.noreply.github.com>
2025-04-03 18:00:15 +0530

    done

--- number_pattern.py
+++ number_pattern.py
@@ -82,3 +82,30 @@ for i in range(n):
         print(p, end = " ")
         p += 1
     print()
+
+#1 
+#2 2 
+#3 3 3 
+#4 4 4 4 
+#5 5 5 5 5
+n = 5
+for i in range(n):
+    p = i +1
+    for j in range(i+1):
+        print(p, end=' ')
+    print( )   
+
+'''
+1 
+2 3 
+3 4 5 
+4 5 6 7 
+5 6 7 8 9'''
+n = 5
+for i in range(n):
+    p = i + 1
+    for j in range(i+1):
+        print(p, end=' ')
+        p += 1
+    print()
+    
