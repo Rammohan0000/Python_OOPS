3539edbaf3e49613c07ab8d39e1269b4c7da840b
Rammohan0000 <120706490+Rammohan0000@users.noreply.github.com>
2025-04-04 11:38:20 +0530

    done

--- examples.py
+++ examples.py
@@ -343,6 +343,21 @@ for i in range(len(l)):
 print("Sorted list:", l)
 
 
-
-
+# count no .of prime number 
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+def count_primes(n):
+    count = 0
+    for i in range(n):
+        if is_prime(i):
+            count += 1
+    return count
+print(count_primes(10))  # Output: 4
+print(count_primes(0))   # Output: 0
+print(count_primes(1))   # Output: 0
 
